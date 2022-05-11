a3cd6597c18681af6e4fa5f0d5bdbfab81fdd881
NguyenLoi <hongloi123123@gmail.com>
2022-05-11 07:22:57 +0700

    6. Adding a sliver app-bar

--- lib/screens/bottom_userInfo.dart
+++ lib/screens/bottom_userInfo.dart
@@ -1,3 +1,4 @@
+import 'package:ECommerceApp/consts/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:list_tile_switch/list_tile_switch.dart';
@@ -9,19 +10,145 @@ class BottomUserInfo extends StatefulWidget {
 
 class _BottomUserInfoState extends State<BottomUserInfo> {
   bool _value = false;
-  @override
+  late ScrollController _scrollController = ScrollController();
+  var top = 0.0;
+
+  Widget _buildFab() {
+    //starting fab position
+    final double defaultTopMargin = 200.0 - 4.0;
+    //pixels from top where scaling should start
+    final double scaleStart = 160.0;
+    //pixels from top where scaling should end
+    final double scaleEnd = scaleStart / 2;
+
+    double top = defaultTopMargin;
+    double scale = 1.0;
+    if (_scrollController.hasClients) {
+      double offset = _scrollController.offset;
+      top -= offset;
+      if (offset < defaultTopMargin - scaleStart) {
+        //offset small => don't scale down
+        scale = 1.0;
+      } else if (offset < defaultTopMargin - scaleEnd) {
+        //offset between scaleStart and scaleEnd => scale down
+        scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
+      } else {
+        //offset passed scaleEnd => hide fab
+        scale = 0.0;
+      }
+    }
+
+return  Positioned(
+      top: top,
+      right: 16.0,
+      child:  Transform(
+        transform:  Matrix4.identity()..scale(scale),
+        alignment: Alignment.center,
+        child:  FloatingActionButton(
+          heroTag: "btn1",
+          onPressed: (){},
+          child:  Icon(Icons.camera_alt_outlined),
+        ),
+      ),
+    );
+  }
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Padding(
-            padding: EdgeInsets.only(left: 8.0),
-            child: userTitle(title: 'User information'),
-          ),
-          Divider(thickness: 1, color: Colors.grey),
-          userListTile(
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: <Widget>[
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                elevation: 4,
+                expandedHeight: 200,
+                pinned: true,
+                flexibleSpace: LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                  top = constraints.biggest.height;
+                  return Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            ColorsConsts.starterColor,
+                            ColorsConsts.endColor,
+                          ],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    child: FlexibleSpaceBar(
+                      collapseMode: CollapseMode.parallax,
+                      centerTitle: true,
+                      title: Row(
+                        //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          AnimatedOpacity(
+                            duration: Duration(milliseconds: 300),
+                            opacity: top <= 110.0 ? 1.0 : 0,
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Container(
+                                  height: kToolbarHeight / 1.8,
+                                  width: kToolbarHeight / 1.8,
+                                  decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.white,
+                                        blurRadius: 1.0,
+                                      ),
+                                    ],
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      fit: BoxFit.fill,
+                                      image: NetworkImage(
+                                          'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Text(
+                                  // 'top.toString()',
+                                  'Guest',
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      background: Image(
+                        image: NetworkImage(
+                            'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: userTitle(title: 'User Information')),
+                    Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                      userListTile(
               title: 'Email',
               subTitle: 'nguyenloi@gmail.com',
               icon: Icons.email),
@@ -35,26 +162,35 @@ class _BottomUserInfoState extends State<BottomUserInfo> {
               icon: Icons.local_shipping),
           userListTile(
               title: 'Joined date', subTitle: 'Date', icon: Icons.watch_later),
-          Padding(
-            padding: EdgeInsets.only(left: 8.0),
-            child: userTitle(title: 'User setting'),
-          ),
-          Divider(thickness: 1, color: Colors.grey),
-          ListTileSwitch(
-            value: _value,
-            leading: Icon(Ionicons.md_moon),
-            onChanged: (value) {
-              setState(() {
-                _value = value;
-              });
-            },
-            visualDensity: VisualDensity.comfortable,
-            switchType: SwitchType.cupertino,
-            switchActiveColor: Colors.indigo,
-            title: Text('Dart theme'),
-          ),
-           userListTile(
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: userTitle(title: 'User settings'),
+                    ),
+                    Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    ListTileSwitch(
+                      value: _value,
+                      leading: Icon(Ionicons.md_moon),
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value;
+                        });
+                      },
+                      visualDensity: VisualDensity.comfortable,
+                      switchType: SwitchType.cupertino,
+                      switchActiveColor: Colors.indigo,
+                      title: Text('Dark theme'),
+                    ),
+                    userListTile(
               title: 'Logout', subTitle: '', icon: Icons.exit_to_app_rounded)
+                  ],
+                ),
+              )
+            ],
+          ),
+       
         ],
       ),
     );

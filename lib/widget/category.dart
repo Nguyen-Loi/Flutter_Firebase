a2e62a53ddc41f92624225824383670b15f30eae
NguyenLoi <hongloi123123@gmail.com>
2022-05-11 16:13:54 +0700

    13. Finished home

--- lib/widget/category.dart
+++ lib/widget/category.dart
@@ -1,78 +1,82 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class CategoryWidget extends StatefulWidget {
-//   CategoryWidget({Key key, this.index}) : super(key: key);
-//   final int index;
+class CategoryWidget extends StatefulWidget {
 
-//   @override
-//   _CategoryWidgetState createState() => _CategoryWidgetState();
-// }
+  final int index;
+  const CategoryWidget({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
 
-// class _CategoryWidgetState extends State<CategoryWidget> {
-//   List<Map<String, Object>> categories = [
-//     {
-//       'categoryName': 'Phones',
-//       'categoryImagesPath': 'assets/images/CatPhones.png',
-//     },
-//     {
-//       'categoryName': 'Clothes',
-//       'categoryImagesPath': 'assets/images/CatClothes.jpg',
-//     },
-//     {
-//       'categoryName': 'Shoes',
-//       'categoryImagesPath': 'assets/images/CatShoes.jpg',
-//     },
-//     {
-//       'categoryName': 'Beauty&Health',
-//       'categoryImagesPath': 'assets/images/CatBeauty.jpg',
-//     },
-//     {
-//       'categoryName': 'Laptops',
-//       'categoryImagesPath': 'assets/images/CatLaptops.png',
-//     },
-//     {
-//       'categoryName': 'Furniture',
-//       'categoryImagesPath': 'assets/images/CatFurniture.jpg',
-//     },
-//     {
-//       'categoryName': 'Watches',
-//       'categoryImagesPath': 'assets/images/CatWatches.jpg',
-//     },
-//   ];
+  @override
+  _CategoryWidgetState createState() => _CategoryWidgetState();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       children: [
-//         Container(
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(10),
-//             image: DecorationImage(
-//               image: AssetImage(categories[widget.index]['categoryImagesPath']),
-//             fit: BoxFit.cover),
-//           ),
-//           margin: EdgeInsets.symmetric(horizontal: 10),
-//           width: 150,
-//           height: 150,
-//         ),
-//         Positioned(
-//           bottom: 0,
-//           left: 10,
-//           right: 10,
-//           child: Container(
-//             padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-//             color: Theme.of(context).backgroundColor,
-//             child: Text(
-//               categories[widget.index]['categoryName'],
-//               style: TextStyle(
-//                 fontWeight: FontWeight.w500,
-//                 fontSize: 18,
-//                 color: Theme.of(context).textSelectionColor,
-//               ),
-//             ),
-//           ),
-//         )
-//       ],
-//     );
-//   }
-// }
+class _CategoryWidgetState extends State<CategoryWidget> {
+  List<Map<String, Object>> categories = [
+    {
+      'categoryName': 'Phones',
+      'categoryImagesPath': 'assets/images/CatPhones.png',
+    },
+    {
+      'categoryName': 'Clothes',
+      'categoryImagesPath': 'assets/images/CatClothes.jpg',
+    },
+    {
+      'categoryName': 'Shoes',
+      'categoryImagesPath': 'assets/images/CatShoes.jpg',
+    },
+    {
+      'categoryName': 'Beauty&Health',
+      'categoryImagesPath': 'assets/images/CatBeauty.jpg',
+    },
+    {
+      'categoryName': 'Laptops',
+      'categoryImagesPath': 'assets/images/CatLaptops.png',
+    },
+    {
+      'categoryName': 'Furniture',
+      'categoryImagesPath': 'assets/images/CatFurniture.jpg',
+    },
+    {
+      'categoryName': 'Watches',
+      'categoryImagesPath': 'assets/images/CatWatches.jpg',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: AssetImage(categories[widget.index]['categoryImagesPath'] as String),
+            fit: BoxFit.cover),
+          ),
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          width: 150,
+          height: 150,
+        ),
+        Positioned(
+          bottom: 0,
+          left: 10,
+          right: 10,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            color: Theme.of(context).backgroundColor,
+            child: Text(
+              categories[widget.index]['categoryName'] as String,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                color: Theme.of(context).textSelectionColor,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
